24eadcb94c630ffb5aae1c646484321f22b64b83
coffee-qm <coffee.qm@gmail.com>
2017-10-19 01:00:26 +0800

    CommonUtils
    CommonUtils

--- src/main/java/com/coffee/common/xml/utils/Dom4jUtils.java
+++ src/main/java/com/coffee/common/xml/utils/Dom4jUtils.java
@@ -1,5 +1,7 @@
 package com.coffee.common.xml.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.dom4j.Document;
@@ -7,6 +9,9 @@ import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import com.coffee.common.common.exp.SvcException;
 
@@ -20,9 +25,27 @@ public final class Dom4jUtils {
 	public Document getDocument(InputStream in) throws SvcException {
 		try {
 			SAXReader reader = new SAXReader();
+			//
+			reader.setEntityResolver(new EntityResolver() {
+				String emptyDtd = "";
+				ByteArrayInputStream byteArrIs = new ByteArrayInputStream(
+						emptyDtd.getBytes());
+
+				@Override
+				public InputSource resolveEntity(String publicId,
+						String systemId) throws SAXException, IOException {
+					return new InputSource(byteArrIs);
+				}
+			});
 			// 
+			reader.setFeature(
+					"http://apache.org/xml/features/disallow-doctype-decl",
+					Boolean.TRUE);
 			return reader.read(in);
 		} catch (DocumentException e) {
+			logger.error("", e);
+			throw new SvcException("Failed to read xml.");
+		} catch (SAXException e) {
 			logger.error("", e);
 			throw new SvcException("Failed to read xml.");
 		}
--- src/main/java/com/coffee/common/xml/utils/JaxUtils.java
+++ src/main/java/com/coffee/common/xml/utils/JaxUtils.java
@@ -2,14 +2,40 @@ package com.coffee.common.xml.utils;
 
 import java.io.InputStream;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.coffee.common.common.exp.SvcException;
 
 /**
  * @author QM
  */
 public final class JaxUtils {
+	
+	private static Logger logger = LoggerFactory.getLogger(JaxUtils.class);
 
+	@SuppressWarnings("unchecked")
 	public <T> T read(InputStream in, Class<T> clazz) throws SvcException {
-		return null;
+		try {
+			final JAXBContext context = JAXBContext.newInstance(clazz);
+			final XMLInputFactory factory = XMLInputFactory.newFactory();
+			factory.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.FALSE);
+			final XMLStreamReader reader = factory.createXMLStreamReader(in);
+			final Unmarshaller unmarshaller = context.createUnmarshaller();
+			return (T) unmarshaller.unmarshal(reader);
+		} catch (JAXBException e) {
+			logger.error("", e);
+			throw new SvcException("Failed to read xml.");
+		} catch (XMLStreamException e) {
+			logger.error("", e);
+			throw new SvcException("Failed to read xml.");
+		}
 	}
 }
