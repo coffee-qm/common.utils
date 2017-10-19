0e03f4d98da97c013a8822522b65618aa1c8068e
coffee-qm <coffee.qm@gmail.com>
2017-10-20 00:40:50 +0800

    CommonUtils
    CommonUtils

--- src/main/java/com/coffee/common/zip/utils/ZipUtils.java
+++ src/main/java/com/coffee/common/zip/utils/ZipUtils.java
@@ -1,11 +1,15 @@
 package com.coffee.common.zip.utils;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.tools.zip.ZipEntry;
 import org.apache.tools.zip.ZipFile;
@@ -22,11 +26,82 @@ import com.coffee.common.io.utils.IoUtils;
 public class ZipUtils {
 
 	private static Logger logger = LoggerFactory.getLogger(ZipUtils.class);
-
+	private static final String SUFFIX = ".zip";
 	private static final int BUFFER_SIZE = 2048;
 
 	/**
-	 * 解压ZIP文件
+	 * 压缩文件
+	 * 
+	 * @param path
+	 *            path
+	 */
+	public static void zip(final String path) {
+		final String zipName = path.substring(0, path.indexOf(File.separator))
+				+ SUFFIX;
+		zip(path, zipName);
+	}
+
+	private static void zip(final String path, final String zipName) {
+		// 创建zip输出流
+		ZipOutputStream out = null;
+		BufferedOutputStream bos = null;
+		try {
+			out = new ZipOutputStream(new FileOutputStream(zipName));
+			// 创建缓冲输出流
+			bos = new BufferedOutputStream(out);
+			final File source = new File(path);
+			// 调用函数
+			compress(out, bos, source, source.getName());
+		} catch (final FileNotFoundException e) {
+			logger.error("", e);
+		} catch (final IOException e) {
+			logger.error("", e);
+		} finally {
+			IoUtils.closeQuietly(bos);
+			IoUtils.closeQuietly(out);
+		}
+	}
+
+	private static void compress(final ZipOutputStream out,
+			final BufferedOutputStream bos, final File sourceFile,
+			final String base) throws IOException {
+		// 如果路径为目录（文件夹）
+		if (sourceFile.isDirectory()) {
+			// 取出文件夹中的文件（或子文件夹）
+			final File[] flist = sourceFile.listFiles();
+			if (flist.length == 0) {
+				// 如果文件夹为空，则只需在目的地zip文件中写入一个目录进入点
+				out.putNextEntry(new ZipEntry(base + "/"));
+			} else {
+				// 如果文件夹不为空，则递归调用compress，文件夹中的每一个文件（或文件夹）进行压缩
+				for (int i = 0; i < flist.length; i++) {
+					compress(out, bos, flist[i],
+							base + "/" + flist[i].getName());
+				}
+			}
+		} else {
+			// 如果不是目录（文件夹），即为文件，则先写入目录进入点，之后将文件写入zip文件中
+			out.putNextEntry(new ZipEntry(base));
+			FileInputStream fos = null;
+			BufferedInputStream bis = null;
+			try {
+				fos = new FileInputStream(sourceFile);
+				bis = new BufferedInputStream(fos);
+				int tag;
+				// 将源文件写入到zip文件中
+				while ((tag = bis.read()) != -1) {
+					bos.write(tag);
+				}
+			} finally {
+				IoUtils.closeQuietly(bis);
+				IoUtils.closeQuietly(fos);
+			}
+
+		}
+	}
+
+	/**
+	 * 解压文件
 	 * 
 	 * @param path
 	 *            path
@@ -40,7 +115,8 @@ public class ZipUtils {
 		BufferedOutputStream bos = null;
 
 		// 保存解压文件目录
-		savepath = path.substring(0, path.lastIndexOf(Symbols.POINT)) + File.separator;
+		savepath = path.substring(0, path.lastIndexOf(Symbols.POINT))
+				+ File.separator;
 		// 创建保存目录
 		new File(savepath).mkdir();
 		try {
@@ -72,7 +148,8 @@ public class ZipUtils {
 				if (!file.exists()) {
 					if (ismkdir) {
 						new File(filename.substring(0,
-								filename.lastIndexOf(Symbols.SLASH_L))).mkdirs();
+								filename.lastIndexOf(Symbols.SLASH_L)))
+								.mkdirs();
 					}
 				}
 				// 创建文件
