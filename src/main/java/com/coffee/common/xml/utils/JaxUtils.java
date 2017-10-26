afe6038f3e3b366d6a533f661dcc10795a8e98f4
coffee-qm <coffee.qm@gmail.com>
2017-10-26 23:52:45 +0800

    CommonUtils
    CommonUtils

--- src/main/java/com/coffee/common/xml/utils/JaxUtils.java
+++ src/main/java/com/coffee/common/xml/utils/JaxUtils.java
@@ -1,13 +1,17 @@
 package com.coffee.common.xml.utils;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,21 +26,48 @@ public final class JaxUtils {
 	private static Logger logger = LoggerFactory.getLogger(JaxUtils.class);
 
 	@SuppressWarnings("unchecked")
-	public <T> T read(final InputStream in, final Class<T> clazz)
+	public static <T> T read(final InputStream in, final Class<T> clazz)
 			throws SvcException {
 		try {
+			//
 			final JAXBContext context = JAXBContext.newInstance(clazz);
+			//
 			final XMLInputFactory factory = XMLInputFactory.newFactory();
 			factory.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.FALSE);
+			//
 			final XMLStreamReader reader = factory.createXMLStreamReader(in);
+			//
 			final Unmarshaller unmarshaller = context.createUnmarshaller();
 			return (T) unmarshaller.unmarshal(reader);
 		} catch (final JAXBException e) {
 			logger.error("", e);
+			e.printStackTrace();
 			throw new SvcException("Failed to read xml.");
 		} catch (final XMLStreamException e) {
 			logger.error("", e);
 			throw new SvcException("Failed to read xml.");
+		}
+	}
+
+	public static <T> void write(final OutputStream out, final Class<T> clazz,
+			final T t) throws SvcException {
+		try {
+			//
+			final JAXBContext context = JAXBContext.newInstance(clazz);
+			//
+			final XMLOutputFactory factory = XMLOutputFactory.newFactory();
+			// factory.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.FALSE);
+			//
+			final XMLStreamWriter writer = factory.createXMLStreamWriter(out);
+			//
+			final Marshaller marshaller = context.createMarshaller();
+			marshaller.marshal(t, writer);
+		} catch (final JAXBException e) {
+			logger.error("", e);
+			throw new SvcException("Failed to write xml.");
+		} catch (final XMLStreamException e) {
+			logger.error("", e);
+			throw new SvcException("Failed to write xml.");
 		}
 	}
 }
