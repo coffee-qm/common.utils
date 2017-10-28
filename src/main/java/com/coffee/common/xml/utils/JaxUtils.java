691ae1b2b937637e9891ed450e88082fc386d262
coffee-qm <coffee.qm@gmail.com>
2017-10-28 23:16:52 +0800

    CommonUtils
    CommonUtils

--- src/main/java/com/coffee/common/xml/utils/JaxUtils.java
+++ src/main/java/com/coffee/common/xml/utils/JaxUtils.java
@@ -1,7 +1,11 @@
 package com.coffee.common.xml.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -13,6 +17,7 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -56,16 +61,41 @@ public final class JaxUtils {
 			final JAXBContext context = JAXBContext.newInstance(clazz);
 			//
 			final XMLOutputFactory factory = XMLOutputFactory.newFactory();
-			// factory.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.FALSE);
 			//
 			final XMLStreamWriter writer = factory.createXMLStreamWriter(out);
 			//
 			final Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 			marshaller.marshal(t, writer);
 		} catch (final JAXBException e) {
 			logger.error("", e);
 			throw new SvcException("Failed to write xml.");
 		} catch (final XMLStreamException e) {
+			logger.error("", e);
+			throw new SvcException("Failed to write xml.");
+		}
+	}
+
+	public static <T> void write(final String path, final Class<T> clazz,
+			final T t) throws SvcException {
+		try {
+			//
+			final JAXBContext context = JAXBContext.newInstance(clazz);
+			//
+			final StringWriter writer = new StringWriter();
+			//
+			final Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			marshaller.marshal(t, new StringWriter());
+			//
+			FileUtils.writeStringToFile(new File(path), writer.toString(),
+					Charset.forName("UTF-8"));
+		} catch (final JAXBException e) {
+			logger.error("", e);
+			throw new SvcException("Failed to write xml.");
+		} catch (final IOException e) {
 			logger.error("", e);
 			throw new SvcException("Failed to write xml.");
 		}
